78f182fd8e576fef31c1e2408a461d5aa60c203b
Lucas <lucbarroco@gmail.com>
2023-04-03 13:03:13 -0300

    Update SQLQuery1.sql

--- Site-exemplo/SQLQuery1.sql
+++ Site-exemplo/SQLQuery1.sql
@@ -1,1 +1,20 @@
-select * from VENDAS_INTERNET
+CREATE OR ALTER VIEW RESULTADOS_ADW AS 
+SELECT
+	fis.SalesOrderNumber AS 'N do Pedido',
+	fis.OrderDate AS 'DATA PEDIDO',
+	dpc.EnglishProductCategoryName AS 'CATEGORIA PRODUTO',
+	fis.customerkey AS 'ID CLIENTE',
+	dc.FirstName + ' ' + dc.LastName AS 'NOME CLIENTE',
+	REPLACE(REPLACE(dc.Gender, 'M', 'MASCULINO'), 'F', 'Feminino')AS 'GENERO',
+	dg.EnglishCountryRegionName AS 'PAIS',
+	fis.OrderQuantity AS 'QTD. VENDIDA',
+	fis.SalesAmount AS 'RECEITA VENDA',
+	fis.TotalProductCost AS 'CUSTO VENDA',
+	fis.SalesAmount - fis.TotalproductCost AS 'LUCRO VENDA'
+FROM
+	FactInternetSales fis
+INNER JOIN DimProduct dp ON fis.ProductKey = dp.ProductKey
+	INNER JOIN DimProductSubcategory dps ON dps.ProductSubcategoryKey = dps.ProductSubcategoryKey
+		INNER JOIN DimProductCategory dpc ON dps.ProductCategoryKey = dpc.ProductCategoryKey
+INNER JOIN DimCustomer dc ON fis.CustomerKey = dc.CustomerKey
+	INNER JOIN DimGeography dg ON dc.GeographyKey = dg.GeographyKey
